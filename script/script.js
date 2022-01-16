f220a1dbc6784af9c63b187226f9373dff9bb8a8
Alexis Bally <alexisbally02@gmail.com>
2022-01-16 23:43:46 +0100

    :building_construction: Add projects and animations

--- script/script.js
+++ script/script.js
@@ -6,3 +6,23 @@ var topbutton = document.getElementById("topbutton");
             behavior: 'smooth'
         });
     }
+
+const ratio = 0.1;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+const handleIntersect = function(entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('trans-visible');
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="trans-"]').forEach(function (t) {
+    observer.observe(t);
+})
